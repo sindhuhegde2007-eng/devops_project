0d9df9e54c8da5d67f855b5683894086c2660d4d
sindhuhegde2007-eng <sindhuhegde2007@gmail.com>
2026-01-06 11:55:54 +0530

    save

--- employee.py
+++ employee.py
@@ -14,20 +14,28 @@ def get_salary_grade(salary):
 
 
 def main():
-    # Command line arguments
-    name = sys.argv[1]
-    emp_id = sys.argv[2]
-    department = sys.argv[3]
-    salary = float(sys.argv[4])
+    # If arguments are passed, use them
+    if len(sys.argv) == 5:
+        name = sys.argv[1]
+        emp_id = sys.argv[2]
+        department = sys.argv[3]
+        salary = float(sys.argv[4])
+    else:
+        # Default values (project requirement – all in code)
+        name = "Sindhu"
+        emp_id = "1001"
+        department = "IT"
+        salary = 80000
 
     grade = get_salary_grade(salary)
 
-    print("\n--- Employee Details ---")
+    print("Employee Details")
+    print("----------------")
     print(f"Name       : {name}")
     print(f"ID         : {emp_id}")
     print(f"Department : {department}")
     print(f"Salary     : {salary}")
-    print(f"Grade      : {grade}")
+    print(f"Salary Grade: {grade}")
 
 
 if __name__ == "__main__":
