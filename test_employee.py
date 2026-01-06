d41bc052818e9ca04ba3fd1007ada94dbd342db8
sindhuhegde2007-eng <sindhuhegde2007@gmail.com>
2026-01-06 11:59:26 +0530

    save

--- test_employee.py
+++ test_employee.py
@@ -1,5 +1,14 @@
-from employee import get_salary_grade
+from emp import employee_details
 
-def test_salary_grade():
-    result = get_salary_grade()
-    assert result is not None
+def test_employee_details():
+    expected_output = (
+        "Employee Details\n"
+        "----------------\n"
+        "Name       : Sindhu\n"
+        "ID         : 1001\n"
+        "Department : IT\n"
+        "Salary     : Rs. 80000\n"
+        "Salary Grade: Grade A"
+    )
+
+    assert employee_details("Sindhu", "1001", "IT", 80000) == expected_output
